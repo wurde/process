c03ea861475192f3a794e4bff25c61dc8691ca43
Andy Bettisworth <andy@accreu.com>
2020-01-14 15:20:45 -0600

    Add Hello resolver

---
+++ resolvers/Hello.resolver.ts
@@ -0,0 +1,23 @@
+/**
+ * Dependencies
+ */
+
+import { Query, Resolver } from 'type-graphql';
+
+/**
+ * Define resolver
+ */
+
+@Resolver()
+class Hello {
+  @Query(() => String)
+  hello() {
+    return 'Hi!';
+  }
+}
+
+/**
+ * Export resolver
+ */
+
+export default Hello;
