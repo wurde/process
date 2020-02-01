79fe300c87068d352a102d4a4ec07d311fcdd30d
Andy Bettisworth <andy@accreu.com>
2020-01-31 21:03:25 -0600

    Add return types

--- server/resolvers/Model.resolver.ts
+++ server/resolvers/Model.resolver.ts
@@ -14,22 +14,22 @@ import { Model } from "../models/Model";
 @Resolver()
 export class ModelResolver {
     @Query(() => [Model])
-    listModels() {
+    listModels(): Promise<Model[]> {
         return Model.find();
     }
 
     @Query(() => Model, { nullable: true })
-    findModelByID(@Arg("id", () => Int) id: number) {
+    findModelByID(@Arg("id", () => Int) id: number): Promise<Model> {
         return Model.findOne({ id });
     }
 
     @Query(() => Model, { nullable: true })
-    findModelByTitle(@Arg("title", () => String) title: string) {
+    findModelByTitle(@Arg("title", () => String) title: string): Promise<Model> {
         return Model.findOne({ title });
     }
 
     @Mutation(() => Model)
-    async createModel(@Arg("input") input: CreateModelInput) {
+    async createModel(@Arg("input") input: CreateModelInput): Promise<Model> {
         let m = new Model();
         m = Object.assign(m, input);
         await m.save();
@@ -40,7 +40,7 @@ export class ModelResolver {
     async updateModel(
         @Arg("id", () => Int) id: number,
         @Arg("input") input: UpdateModelInput
-    ) {
+    ): Promise<Model | null>{
         let m = await Model.findOne(id);
         if (m) {
             m = Object.assign(m, input);
@@ -51,7 +51,7 @@ export class ModelResolver {
     }
 
     @Mutation(() => Boolean)
-    async removeModel(@Arg("id", () => Int) id: number) {
+    async removeModel(@Arg("id", () => Int) id: number): boolean {
         let m = await Model.findOne(id);
         if (m) {
             await m.remove();
